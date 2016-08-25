5449854481802c319a21ad3e898a3e0baa54b615
sdarekar <sdarekar@admin-PC>
2016-08-25 09:20:37 +0530

    first commit

---
+++ src/Coffee/Dark.java
@@ -0,0 +1,19 @@
+package Coffee;
+
+public class Dark {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("your coffee type is:");
+	System.out.println("Coffee beans are called:");
+	Dark softDark= new Dark("Hot",20);
+
+	}
+	
+	public Dark(String name,int no)
+	{
+		System.out.println("your coffee name is"+name);
+		System.out.println("your type is"+no);
+	}
+
+}
---
+++ src/Coffee/Tea.java
@@ -0,0 +1,16 @@
+package Coffee;
+
+public class Tea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Tea hotTea=new Tea("Quantity");
+
+	}
+	
+	public Tea(String name)
+	{
+		System.out.println("your tea type is:"+ name);
+	}
+
+}
